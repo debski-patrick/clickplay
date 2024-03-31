30799b146c4e72ac6c5d936ad5d2125d561916be
Debski Patrick <mathiassevenfold217@gmail.com>
2024-03-31 10:48:54 -0400

    upload source code

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Helmet from "../../layouts/Helmet/Helmet";
+
+export default function About() {
+  const description =
+    "We value your feedback and are eager to hear from you. Our contact page is designed to provide you with a convenient way to reach out to us. Whether you have a question, suggestion, or simply want to say hello, we encourage you to get in touch.";
+  const title = "ClickPlay - Contact";
+
+  return (
+    <div className="contact">
+      <Helmet title={title} description={description} />
+      <div className="contact-body">
+        <h1 className="more-pages-title">Contact Us</h1>
+
+        <div className="contact-us-email">
+          Please reach out to us, whether you have questions, suggestions, or
+          simply want to send a friendly greeting. We warmly encourage you to
+          get in touch, as your feedback is highly valuable to us. Contact us on
+          <span style={{ fontWeight: "600" }}> assist AT techie DOT com</span>
+        </div>
+      </div>
+    </div>
+  );
+}
